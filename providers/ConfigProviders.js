e979088ec5b59aa4a29e1bcc61b0889efc761c3a
Abhay Mishra <abhaym1205@gmail.com>
2024-11-04 22:01:32 +0530

    added font and applied persistent redux toolkit

---
+++ providers/ConfigProviders.js
@@ -0,0 +1,18 @@
+import Progress from '@/components/common/progress/Progress';
+import React from 'react';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "@/lib/store/store.js";
+
+const ConfigProviders = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+        <Progress />
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default ConfigProviders;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,43 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ["var(--font-montserrat)"],
+        openSans: ["var(--font-open-sans)"],
+        poppins: ["var(--font-poppins)"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#003366",
+        heading: "#161C2D",
+        heading_Blue_BG: "#E0E0E0",
+        subheading: "#4D4D4D",
+        subheading_Blue_BG: "#B3B3B3",
+        paragraph: "#2F2F2F",
+        Paragraph_Primary_BG: "#DDDDDD",
+        border: "#CCCCCC",
+        Border_Primary_BG: "#333333",
+        Nav_Link_Primary_BG: "##FF9B3F",
+        textSecond: "#5c616c",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 16s linear infinite",
+      },
+      screens: {
+        'md_desktop': '1200px',
+        'sm_desktop': '1100px',
+        'xsm_desktop': '1060px',
+        'md_phone': '530px',
+        'phone': "470px",
+        'sm_phone': '380px',
       },
     },
+
   },
   plugins: [],
 };
